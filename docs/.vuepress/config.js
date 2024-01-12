89f3d6e2eb23b0002ef91f1b93fc57115723ac83
emiyl <me@emiyl.com>
2024-01-12 17:04:49 +0000

    update vuepress and theme

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,14 +1,10 @@
-const { localePath, locales, themeConfigLocales, searchLocales } = require("./i18n")
-const container = require('markdown-it-container')
-const { path, fs } = require('@vuepress/utils')
-const { searchPlugin } = require('@vuepress/plugin-search')
-const { registerComponentsPlugin } = require('@vuepress/plugin-register-components')
-const { localTheme } = require('../../emiyl-theme')
+const { locales, themeConfigLocales } = require("./i18n")
+const { emiylTheme } = require('../../emiyl-theme-2')
 
 module.exports = {
   locales: locales,
   
-  theme: localTheme({
+  theme: emiylTheme({
     repo: 'cfw-guide/cemu.cfw.guide',
     selectLanguageText: '<i class="fas fa-globe"></i>',
     locales: themeConfigLocales,
@@ -17,13 +13,6 @@ module.exports = {
       "8442"
     ]
   }),
-
-	plugins: [
-    searchPlugin({locales: searchLocales}),
-    registerComponentsPlugin({
-      componentsDir: path.resolve(__dirname, './components'),
-    }),
-	],
   
   head: [
     ['link', { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.6.1/css/all.css' } ],
@@ -32,30 +21,4 @@ module.exports = {
   ],
 
   shouldPrefetch: true,
-
-  extendsMarkdown: (md) => {
-    md.use(require('markdown-it-include'))
-
-    md.use(container, "tabs", {
-      render: (tokens, idx) => {
-        const token = tokens[idx];
-        if (token.nesting === 1) {
-          return `<Tabs ${token.info}>\n`;
-        } else {
-          return `</Tabs>\n`;
-        }
-      }
-    });
-  
-    md.use(container, 'tab', {
-      render: (tokens, idx) => {
-        const token = tokens[idx];
-        if (token.nesting === 1) {
-          return `<Tab ${token.info}>\n`;
-        } else {
-          return `</Tab>\n`;
-        }
-      }
-    });
-  },
 }
